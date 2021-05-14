2a51308dcf8cf480b336aed3edb2976689831e40
AleezaKhan123 <81834587+AleezaKhan123@users.noreply.github.com>
2021-05-14 15:15:49 -0700

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class paper{
+       constructor(x,y,r);
+
+       var options={
+           isStatic:false,
+           restitution:0.3,
+           friction:2,
+           density:1.2;
+
+       }
+    }
